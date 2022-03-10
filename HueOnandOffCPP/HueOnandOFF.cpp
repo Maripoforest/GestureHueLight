6b7314644537f43d06f4ced6d81710171629f5c2
Xiangmin Xu <65858311+Maripoforest@users.noreply.github.com>
2022-03-10 00:38:04 +0000

    Update HueOnandOFF.cpp

--- HueOnandOffCPP/HueOnandOFF.cpp
+++ HueOnandOffCPP/HueOnandOFF.cpp
@@ -4,12 +4,6 @@ A Hue static IP and static user on/off brightness control method.
 Relies on library cpr https://github.com/libcpr/cpr.git, cpr has some C++ HTTPS method that can be used to send message to the Hue light bulb api.
 */
 
-/*
-Arthor: Xiangmin XU (Maripoforest)
-A Hue static IP and static user on/off brightness control method.
-Relies on library cpr https://github.com/libcpr/cpr.git, cpr has some C++ HTTPS method that can be used to send message to the Hue light bulb api.
-*/
-
 #include <cpr/cpr.h>
 #include <iostream>
 #include <string>
