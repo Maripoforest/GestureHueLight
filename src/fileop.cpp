02feb2d0def0cf22e8a0169aaa5aa5e0e8f1e9a8
Xiangmin Xu <65858311+Maripoforest@users.noreply.github.com>
2022-03-21 11:11:33 +0000

    Update fileop.cpp

--- src/fileop.cpp
+++ src/fileop.cpp
@@ -1,3 +1,9 @@
+/*
+Arthor: Xiangmin XU (Maripoforest) Haiyang You (rhythm232) 2022
+A Hue static IP and static user on/off brightness control method.
+Relies on library cpr https://github.com/libcpr/cpr.git, cpr has some C++ HTTPS method that can be used to send message to the Hue light bulb api.
+*/
+
 #include <iostream>
 #include <string>
 #include <vector>
